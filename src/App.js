39e84ccc55f72b108a5efc47ad23494cdc5d9bb8
Vitor <vitorm.ramires@hotmail.com>
2021-04-21 11:00:25 -0300

    Comittar

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import Conversor from './componentes/Conversor.js'
+import  "./componentes/Conversor.css"
+
+function App() {
+  return (
+    <div className="App">
+       <h1>Conversor de moedas!</h1>
+    <div className="linha">
+      <Conversor  moedaA="USD" moedaB="BRL"/>
+      <Conversor  moedaA="BRL" moedaB="USD"/>
+    </div>
+    <div className="linha">
+      <Conversor  moedaA="CAD" moedaB="BRL"/>
+      <Conversor  moedaA="BRL" moedaB="CAD"/>
+    </div>
+    <div className="linha">
+      <Conversor  moedaA="EUR" moedaB="BRL"/>
+      <Conversor  moedaA="BRL" moedaB="EUR"/>
+    </div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/Conversor.js
@@ -0,0 +1,49 @@
+import React, {Component} from "react"
+
+export default class Conversor extends Component{
+
+constructor(props){
+    super(props);
+    this.state = {
+        moedaA_valor:"",
+        moedaB_valor:0,
+    } //o state serve para armazenarmos os dados, para usarmos depois
+
+    this.converter = this.converter.bind(this)
+
+} // para acessar as propriedades usadas no App.js
+// devemos usar o this, para nos referir a uma propriedade usadana nossa classe
+
+converter(){
+
+   let de_para = `${this.props.moedaA}_${this.props.moedaB}`
+   let url = `https://free.currconv.com/api/v7/convert?q=${de_para}&compact=ultra&apiKey=69c0f633a3bcfd12bbde`
+
+
+   fetch(url)
+    .then(res => {
+        return res.json()
+    })
+    .then(json => {
+        let cotacao = json[de_para];
+        let moedaB_valor = (parseFloat (this.state.moedaA_valor) * cotacao).toFixed(2)
+        this.setState({moedaB_valor})
+    })
+
+}
+
+
+render(){
+return(
+    <div className = "conversor">
+    <p>{this.props.moedaA} para {this.props.moedaB}</p>
+    <input type="text" onChange={(event)=>{this.setState({moedaA_valor:event.target.value})}}></input>
+
+    <button type="button" value="Converter" onClick = {this.converter}>Converter</button>
+    <h2>{this.state.moedaB_valor}</h2>
+    </div>
+)
+} //render
+}// final do componente
+
+
