392b0a23cc9bbb378297ed07de34c81f761b5d1c
Vitor <vitorm.ramires@hotmail.com>
2021-04-26 08:22:10 -0300

    Tratamento de erro

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import  "./componentes/Conversor.css"
 function App() {
   return (
     <div className="App">
-       <h1>Conversor de moedas!</h1>
+       <h1>Conversor de moedas</h1>
     <div className="linha">
       <Conversor  moedaA="USD" moedaB="BRL"/>
       <Conversor  moedaA="BRL" moedaB="USD"/>
--- src/componentes/Conversor.js
+++ src/componentes/Conversor.js
@@ -16,6 +16,11 @@ constructor(props){
 
 converter(){
 
+    let inputText = document.getElementById('texto')
+    if(inputText.value.length === 0){
+        alert('Preencha o campo')
+    } else {
+
    let de_para = `${this.props.moedaA}_${this.props.moedaB}`
    let url = `https://free.currconv.com/api/v7/convert?q=${de_para}&compact=ultra&apiKey=69c0f633a3bcfd12bbde`
 
@@ -29,6 +34,7 @@ converter(){
         let moedaB_valor = (parseFloat (this.state.moedaA_valor) * cotacao).toFixed(2)
         this.setState({moedaB_valor})
     })
+}
 
 }
 
@@ -37,7 +43,7 @@ render(){
 return(
     <div className = "conversor">
     <p>{this.props.moedaA} para {this.props.moedaB}</p>
-    <input type="text" onChange={(event)=>{this.setState({moedaA_valor:event.target.value})}}></input>
+    <input type="text" id="texto" onChange={(event)=>{this.setState({moedaA_valor:event.target.value})}}></input>
 
     <button type="button" value="Converter" onClick = {this.converter}>Converter</button>
     <h2>{this.state.moedaB_valor}</h2>
